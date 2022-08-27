5a33cfcf522321fb203bf051f141ded0e6fa9c09
GreedNeSS <ruslan199126@gmail.com>
2022-08-27 21:04:29 +0300

    Added IndexViewModel.

---
+++ WiFi-Antennas/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WiFi_Antennas.Models
+{
+    public class IndexViewModel
+    {
+        public List<AntennaViewModel> Antennas { get; set; }
+
+        public IndexViewModel(List<AntennaViewModel> antennas)
+        {
+            Antennas = antennas;
+        }
+    }
+}
