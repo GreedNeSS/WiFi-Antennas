38a5e8c486c56b8b1a6f0cb68d1283ddf22e165b
GreedNeSS <ruslan199126@gmail.com>
2022-08-28 23:20:57 +0300

    Added sort state.

---
+++ WiFi-Antennas.DAL/EF/SortState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFi_Antennas.DAL.EF
+{
+    public enum SortState
+    {
+        IpAsc,
+        IpDesc,
+        AddressAsc,
+        AddressDesc,
+        SSIDAsc,
+        SSIDDesc,
+    }
+}
