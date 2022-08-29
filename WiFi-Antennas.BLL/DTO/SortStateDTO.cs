f716b5724b7e00010cc8a2f45c807119a022bf86
GreedNeSS <ruslan199126@gmail.com>
2022-08-29 22:59:46 +0300

    Added SortStateDTO.

---
+++ WiFi-Antennas.BLL/DTO/SortStateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFi_Antennas.BLL.DTO
+{
+    public enum SortStateDTO
+    {
+        IpAsc,
+        IpDesc,
+        AddressAsc,
+        AddressDesc,
+        SSIDAsc,
+        SSIDDesc,
+    }
+}
