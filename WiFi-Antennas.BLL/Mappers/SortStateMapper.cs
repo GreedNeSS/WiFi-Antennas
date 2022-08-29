a7dd2399110b5a6a8919efc005d7075fb213dfd9
GreedNeSS <ruslan199126@gmail.com>
2022-08-29 23:12:33 +0300

    Added SortState Mapper from DAL.

---
+++ WiFi-Antennas.BLL/Mappers/SortStateMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiFi_Antennas.BLL.DTO;
+using WiFi_Antennas.DAL.EF;
+
+namespace WiFi_Antennas.BLL.Mappers
+{
+    public static class SortStateMapper
+    {
+        public static SortState ToDAL(this SortStateDTO sortStateDTO)
+        {
+            SortState sortState = sortStateDTO switch
+            {
+                SortStateDTO.AddressAsc => SortState.AddressAsc,
+                SortStateDTO.AddressDesc => SortState.AddressDesc,
+                SortStateDTO.SSIDAsc => SortState.SSIDAsc,
+                SortStateDTO.SSIDDesc => SortState.SSIDDesc,
+                SortStateDTO.IpAsc => SortState.IpAsc,
+                SortStateDTO.IpDesc => SortState.IpDesc,
+                _ => SortState.IpAsc,
+            };
+
+            return sortState;
+        }
+    }
+}
