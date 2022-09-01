5686862f71307bbf7df429e2df8495f9de6e90b4
GreedNeSS <ruslan199126@gmail.com>
2022-09-01 18:22:02 +0300

    Added SortStateViewModel.

---
+++ WiFi-Antennas/Models/SortStateViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WiFi_Antennas.Models
+{
+    public enum SortStateViewModel
+    {
+        IpAsc,
+        IpDesc,
+        AddressAsc,
+        AddressDesc,
+        SSIDAsc,
+        SSIDDesc,
+    }
+}
