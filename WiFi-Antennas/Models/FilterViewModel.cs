b9233a63ee9d953ab222e5f79c1ebab4740594d0
GreedNeSS <ruslan199126@gmail.com>
2022-09-04 21:22:54 +0300

    Added FilterViewModel.

---
+++ WiFi-Antennas/Models/FilterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WiFi_Antennas.Models
+{
+    public class FilterViewModel
+    {
+        public string? IpFiltering { get; set; }
+        public string? SSIDFiltering { get; set; }
+        public string? AddressFiltering { get; set; }
+    }
+}
