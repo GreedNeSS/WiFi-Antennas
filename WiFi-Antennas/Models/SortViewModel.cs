fb66f51ab2339327b911f34eb63eacd05a0e7f9e
GreedNeSS <ruslan199126@gmail.com>
2022-09-04 21:59:04 +0300

    Added SortViewModel.

---
+++ WiFi-Antennas/Models/SortViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace WiFi_Antennas.Models
+{
+    public class SortViewModel
+    {
+        public SortStateViewModel IpSort { get; set; }
+        public SortStateViewModel SSIDSort { get; set; }
+        public SortStateViewModel AddressSort { get; set; }
+        public SortStateViewModel Current { get; set; }
+        public bool Up { get; set; }
+
+        public SortViewModel(SortStateViewModel sortOrder)
+        {
+            IpSort = SortStateViewModel.IpAsc;
+            SSIDSort = SortStateViewModel.SSIDAsc;
+            AddressSort = SortStateViewModel.AddressAsc;
+            Up = sortOrder != SortStateViewModel.IpDesc &&
+                sortOrder != SortStateViewModel.SSIDDesc &&
+                sortOrder != SortStateViewModel.AddressDesc;
+            Current = sortOrder;
+
+            switch (sortOrder)
+            {
+                case SortStateViewModel.IpDesc:
+                    IpSort = SortStateViewModel.IpAsc;
+                    break;
+                case SortStateViewModel.AddressAsc:
+                    AddressSort = SortStateViewModel.AddressDesc;
+                    break;
+                case SortStateViewModel.AddressDesc:
+                    AddressSort = SortStateViewModel.AddressAsc;
+                    break;
+                case SortStateViewModel.SSIDAsc:
+                    SSIDSort = SortStateViewModel.SSIDDesc;
+                    break;
+                case SortStateViewModel.SSIDDesc:
+                    SSIDSort = SortStateViewModel.SSIDAsc;
+                    break;
+                default:
+                    IpSort = SortStateViewModel.IpDesc;
+                    break;
+            }
+        }
+    }
+}
