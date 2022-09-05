0cda2b055b6b599c7465359d9407ab41867a0c8f
GreedNeSS <ruslan199126@gmail.com>
2022-09-05 18:47:42 +0300

    Edited IndexViewModel.

--- WiFi-Antennas/Models/IndexViewModel.cs
+++ WiFi-Antennas/Models/IndexViewModel.cs
@@ -3,10 +3,16 @@
     public class IndexViewModel
     {
         public List<AntennaViewModel> Antennas { get; set; }
+        public FilterViewModel Filter { get; set; }
+        public SortViewModel Sort { get; set; }
+        public PageViewModel Pagination { get; set; }
 
-        public IndexViewModel(List<AntennaViewModel> antennas)
+        public IndexViewModel(List<AntennaViewModel> antennas, FilterViewModel filter, SortViewModel sort, PageViewModel pagination)
         {
             Antennas = antennas;
+            Filter = filter;
+            Sort = sort;
+            Pagination = pagination;
         }
     }
 }
