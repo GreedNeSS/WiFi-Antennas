0121ce64fb9af634eeef1a69f3345ff68d760422
GreedNeSS <ruslan199126@gmail.com>
2022-09-11 13:56:21 +0300

    Added InfoController.

---
+++ WiFi-Antennas/Controllers/InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WiFi_Antennas.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Channels()
+        {
+            return View();
+        }
+    }
+}
