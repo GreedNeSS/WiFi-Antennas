c9a56a5449a20a13b79793824a80423b50cea7a5
GreedNeSS <ruslan199126@gmail.com>
2022-12-07 20:58:30 +0300

    Edited DTO model.

--- WiFi-Antennas.BLL/DTO/AntennaDTO.cs
+++ WiFi-Antennas.BLL/DTO/AntennaDTO.cs
@@ -12,7 +12,12 @@ namespace WiFi_Antennas.BLL.DTO
         public int Id { get; set; }
         public AddressDTO Address { get; set; } = new AddressDTO();
         public string? SSID { get; set; }
-        public string? Ip { get; set; }
+
+        public string? StringIP { get; set; }
+        public byte Ip1 { get; set; }
+        public byte Ip2 { get; set; }
+        public byte Ip3 { get; set; }
+        public byte Ip4 { get; set; }
         public bool IsServer { get; set; }
         public string? Login { get; set; }
         public string? AccessPointPassword { get; set; }
