f31351f0d813c8bf51623af94558ef4de58a535e
GreedNeSS <ruslan199126@gmail.com>
2022-12-07 20:56:59 +0300

    Added validation.

--- WiFi-Antennas/Models/AntennaViewModel.cs
+++ WiFi-Antennas/Models/AntennaViewModel.cs
@@ -14,17 +14,17 @@ namespace WiFi_Antennas.Models
         public string? SSID { get; set; }
 
         public string? StringIP { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
-        [Range(0, 255, ErrorMessage = "Значение каждого поля IP адреса должны быть в пределах от 0 до 255")]
+        [Required(ErrorMessage = "Поле 1 обязательно к заполнению!")]
+        [Range(0, 255, ErrorMessage = "Значение первого поля IP адреса должно быть в пределах от 0 до 255")]
         public byte Ip1 { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
-        [Range(0, 255, ErrorMessage = "Значение каждого поля IP адреса должны быть в пределах от 0 до 255")]
+        [Required(ErrorMessage = "Поле 2 обязательно к заполнению!")]
+        [Range(0, 255, ErrorMessage = "Значение второго поля IP адреса должно быть в пределах от 0 до 255")]
         public byte Ip2 { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
-        [Range(0, 255, ErrorMessage = "Значение каждого поля IP адреса должны быть в пределах от 0 до 255")]
+        [Required(ErrorMessage = "Поле 3 обязательно к заполнению!")]
+        [Range(0, 255, ErrorMessage = "Значение третьего поля IP адреса должно быть в пределах от 0 до 255")]
         public byte Ip3 { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
-        [Range(0, 255, ErrorMessage = "Значение каждого поля IP адреса должны быть в пределах от 0 до 255")]
+        [Required(ErrorMessage = "Поле 4 обязательно к заполнению!")]
+        [Range(0, 255, ErrorMessage = "Значение четвертого поля IP адреса должно быть в пределах от 0 до 255")]
         public byte Ip4 { get; set; }
         public bool IsServer { get; set; }
 
